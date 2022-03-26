45b3f06b9adb47aa4529e91427e9cab366cdda70
costanza <costanza.casullo@hotmail.it>
2022-03-26 19:43:19 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, {});
+  });
+
+    document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(elems, {});
+  });
